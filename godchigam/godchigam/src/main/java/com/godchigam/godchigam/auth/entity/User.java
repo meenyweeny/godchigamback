b7ab500225904a36d762081d4ae8c41b4ea2794f
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2024-01-16 16:17:10 +0900

    feat #2 : User entity 추가

--- godchigam/godchigam/src/main/java/com/godchigam/godchigam/auth/entity/User.java
+++ godchigam/godchigam/src/main/java/com/godchigam/godchigam/auth/entity/User.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -19,6 +20,25 @@ public class User {
 
     @Id
     @GeneratedValue
-    @Column(name="USER_ID")
+    @Column(name="user_id")
     private Long userId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column
+    private String address;
+
+    @Column(name="profile_image_url", nullable = false)
+    @ColumnDefault("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973461_960_720.png")
+    private String profileImageUrl;
+
+    @Column(name = "is_active", columnDefinition = "boolean default true")
+    private Boolean isActive;
 }
